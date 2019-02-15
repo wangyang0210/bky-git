49657504bcf3f8b0e07661b0f8cd8407d5127065
wyy <2752154874@qq.com>
2019-02-15 11:05:57 +0800

    webpack-ryf

---
+++ webpack-ryf/webpack-demos/demo05/main.js
@@ -0,0 +1,7 @@
+var img1 = document.createElement("img");
+img1.src = require("./small.png");
+document.body.appendChild(img1);
+
+var img2 = document.createElement("img");
+img2.src = require("./4853ca667a2b8b8844eb2693ac1b2578.png");
+document.body.appendChild(img2);
